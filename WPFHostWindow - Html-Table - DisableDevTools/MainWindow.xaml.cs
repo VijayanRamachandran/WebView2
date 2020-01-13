e661ead14495b0c4b31d2169ce9aaa843145b56f
VijayanRamachandran <ramachandran.vijayan.r@gmail.com>
2020-01-13 12:45:52 +0530

    Updated with Project to perform URL Navigation

---
+++ WPFHostWindow - Html-Table - DisableDevTools/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WPFHostWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WebView2BrowserWrapper.WebView2BrowserWrapper myWebView2BrowserWrapper1;
+        private Stopwatch myStopWatch = new Stopwatch();
+        public MainWindow()
+        {
+            myStopWatch.Start();
+
+            InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            var width = Application.Current.MainWindow.ActualWidth;
+            var height = Application.Current.MainWindow.ActualHeight;            
+
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)?.Replace("file:\\", "");
+
+            myWebView2BrowserWrapper1 = new WebView2BrowserWrapper.WebView2BrowserWrapper(width, 
+                                                                                          height/2,
+                                                                                          path + "\\TableExample.html");
+            myWebView2BrowserWrapper1.OnTabNavigationCompleted += OnTabNavigationCompleted;
+            ControlHostElement1.Child = myWebView2BrowserWrapper1;            
+        }
+
+        private void OnTabNavigationCompleted()
+        {
+            string source = "WebView2UserControl";
+            string log = "Application";
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+
+            myStopWatch.Stop();
+
+            var difference = myStopWatch.ElapsedMilliseconds;
+
+            string message = "HTML - Table - OnTabNavigationCompleted; Duration In MilliSeconds - " + difference;
+            EventLog.WriteEntry(source, message, EventLogEntryType.Warning);
+        }
+
+        private void OnPostScriptClick(object sender, RoutedEventArgs e)
+        {
+            var script1 = "var canvas = document.getElementById(\"ImageCanvas1\");" + 
+                         "var ctx = canvas.getContext(\"2d\");" +
+                         "var img = document.getElementById(\"source1\");" + 
+                         "ctx.drawImage(img, 0, 0);";
+
+            var script2 = "var canvas = document.getElementById(\"ImageCanvas2\");" +
+                            "var ctx = canvas.getContext(\"2d\");" +
+                            "var img = document.getElementById(\"source2\");" +
+                            "ctx.drawImage(img, 0, 0);";
+
+            myWebView2BrowserWrapper1.PostScript(script1);
+            myWebView2BrowserWrapper1.PostScript(script2);
+        }
+    }
+}
---
+++ WPFHostWindow - Html-Table - UrlNavigation/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WPFHostWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WebView2BrowserWrapper.WebView2BrowserWrapper myWebView2BrowserWrapper1;
+        private Stopwatch myStopWatch = new Stopwatch();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            var width = Application.Current.MainWindow.ActualWidth;
+            var height = Application.Current.MainWindow.ActualHeight;                        
+
+            myWebView2BrowserWrapper1 = new WebView2BrowserWrapper.WebView2BrowserWrapper(width, 
+                                                                                          height/2,
+                                                                                          "https://www.google.com");            
+            ControlHostElement1.Child = myWebView2BrowserWrapper1;            
+        }
+
+        private void OnTabNavigationCompleted()
+        {
+            string source = "WebView2UserControl";
+            string log = "Application";
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+
+            myStopWatch.Stop();
+
+            var difference = myStopWatch.ElapsedMilliseconds;
+
+            string message = "HTML - Table - OnTabNavigationCompleted; Duration In MilliSeconds - " + difference;
+            EventLog.WriteEntry(source, message, EventLogEntryType.Warning);
+
+            myWebView2BrowserWrapper1.OnTabNavigationCompleted -= OnTabNavigationCompleted;
+        }
+
+        private void Navigate_Click(object sender, RoutedEventArgs e)
+        {
+            myWebView2BrowserWrapper1.OnTabNavigationCompleted += OnTabNavigationCompleted;
+
+            myStopWatch.Start();
+
+            var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)?.Replace("file:\\", "");
+            myWebView2BrowserWrapper1.Navigate(path + "\\TableExample.html");
+        }
+    }
+}
